b4ba26aa2d74bff39e219043a959ef7e2464cd54
krisine <kristine.nyaga@student.moringaschool.com>
2024-06-30 09:32:57 +0300

    displaying the response

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,4 +1,3 @@
-import { set } from 'date-fns';
 import { useState, useEffect, useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
@@ -93,9 +92,9 @@ const Search = ({ selectedWord, handleSearch, translation }) => {
         <div>
           <p className='tracking-wide text-blue-800 font-medium text-center mb-3'><span className='font-semibold text-lg'>English</span> Explanation</p>
           <div className='bg-gray-100 text-gray-700 border border-gray-300 overflow-y-hidden w-[500px] h-[400px] rounded-md'>
-            {response && (
+
               <div className='p-4 tracking-wide leading-7 h-[100%] overflow-y-auto scrollcontainer'>{response}</div>
-            )}
+
           </div>
         </div>
       </div>
